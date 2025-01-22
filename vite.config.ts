75b78b73bab698ca8dc5932475c945cd728eabf2
razmiO <thomas.cuesta31@gmail.com>
2025-01-22 18:04:01 +0100

    Fix the entry path in vite.config.ts.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import build from "@hono/vite-build/cloudflare-pages";
 import devServer from "@hono/vite-dev-server";
 import adapter from "@hono/vite-dev-server/cloudflare";
+// import ssg from "@hono/vite-ssg";
 import { defineConfig } from "vite";
 
 export default defineConfig(({ mode }) => {
@@ -19,10 +20,11 @@ export default defineConfig(({ mode }) => {
         return {
             plugins: [
                 devServer({
-                    entry: "src/index.tsx",
+                    entry: "./src/index.tsx",
                     adapter, // Cloudflare Adapter
                 }),
                 build(),
+                // ssg(),
             ],
         };
     }
